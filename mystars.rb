6771909cb6b552af104cd0e6384f32608d3308ab
Donald Smith <donald@IIF-Dev.local>
2017-08-09 15:42:26 -0400

    First commit

---
+++ mystars.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/ruby -w
+# encoding: utf-8
+
+require 'date'
+
+class Numeric
+  def to_rad
+    self * Math::PI / 180
+  end
+end
+
+class MyStars
+  attr_accessor :jd, :jda, :t, :gmst, :gast, :last, :lat
+  def initialize(local_lon, local_lat)
+    # Local latitude setter, north is positive
+    @lat = local_lat
+    # Express local_lon as negative for west, positive for east
+    # Current Julian Day, fractional
+    @jd = DateTime.now.ajd.to_f
+    # Julian Days since 1 Jan 2000 at 12 UTC
+    @jda = @jd - 2451545.0
+    # Julian centuries since 1 Jan 2000 at 12 UTC
+    # Currently not using this in favor of a quick calculation, w/ loss of 0.1 sec / century.
+    @t = @jda / 36525.0
+    # Greenwhich Mean Sidereal Time
+    # Quick and dirty version, with loss as mentioned above
+    @gmst = 18.697374558 + ( 24.06570982441908 * @jda )
+    # Reduce to a range of 0 - 24 h
+    @gmst = @gmst % 24
+    # Greenwich Apparent Sidereal Time
+    omega = 125.04 - 0.052954 * @jda
+    l = 280.47 + 0.98565 * @jda
+    epsilon = 23.4393 - 0.0000004 * @jda
+    deltapsi = ( -0.000319 * Math::sin(omega.to_rad) ) - ( 0.000024 * Math::sin( (2*l).to_rad ) )
+    eqeq = deltapsi * Math.cos(epsilon.to_rad)
+    @gast = @gmst + eqeq
+    # Local Apparent Sidereal Time
+    @last = @gast + ( local_lon / 15.0 )
+  end
+
+  def altitude(ra, dec)
+    h = ( @last - ra ) * 15
+    Math::asin( Math::sin(dec.to_rad) * Math::sin(@lat.to_rad) + Math::cos(dec.to_rad) * Math::cos(@lat.to_rad) * Math::cos(h.to_rad) )
+  end
+
+  def azimuth
+
+  end
+end
