49f5f00e207bed174865d95dfff61cc8552da83c
Donald Smith <donald@IIF-Dev.local>
2017-09-07 15:43:29 -0400

    WVP matrices calculating correctly

--- 3d_engine.rb
+++ 3d_engine.rb
@@ -40,10 +40,25 @@ module Stars3D
   end
   # Creates a view matrix based on camera position tx,ty,tz
   # And z axis rotated by rx,ry, rz
-  def view(tx,ty,tz,rx,ry)
-    self.translate(-tx,-ty,-tz) *
-    self.rotate_z(-rz) *
+  # Actually this -applies- the view matrix to an object - this was done for
+  # testing, clean this up to just make the matrix.
+  def view(tx,ty,tz,rx,ry,rz)
+    self.rotate_x(-rx) *
     self.rotate_y(-ry) *
-    self.rotate_x(-rx)
+    self.rotate_z(-rz) *
+    self.translate(-tx,-ty,-tz)
+  end
+  # Perspective projection matrix based on field of view in radians fw and fh
+  # and near and far clipping panes zn and zf
+  def projection(fw, fh, zn, zf)
+    w = 1 / Math.tan(fw/2)
+    h = 1 / Math.tan(fh/2)
+    q = zf / (zf - zn)
+    Matrix[
+      [w, 0, 0, 0      ],
+      [0, h, 0, 0      ],
+      [0, 0, q, -q * zn],
+      [0, 0, 1, 0      ]
+    ]
   end
 end
