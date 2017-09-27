ddfbbefd4c746a1da95e898c155e24a4238b7036
Donald Smith <minstrel@orcforge.net>
2017-09-27 10:02:29 -0400

    info win written, deploy next

--- lib/mystars_info_window.rb
+++ lib/mystars_info_window.rb
@@ -172,206 +172,31 @@ class MyStarsInfoWindow < MyStarsWindow
     @window.refresh
   end
 
-end
-
-# TODO
-# Incorporate these methods into this new class, calling them on @window
-
-
-
-
-
-
-
-
-def self.updateMag
-  info_win = App::INFO_WIN
-  info_win.setpos(2,0)
-  info_win.clrtoeol
-  info_win.addstr(App::Settings.mag.to_s + " degrees")
-  info_win.refresh
-end
-
-def self.updateVisMag
-  info_win = App::INFO_WIN
-  info_win.setpos(4,3)
-  info_win.clrtoeol
-  info_win.addstr(App::Settings.vis_mag.to_s) 
-  info_win.refresh
-end
-
-def self.updateLon
-  info_win = App::INFO_WIN
-  info_win.setpos(33,0)
-  info_win.clrtoeol
-  info_win.addstr(App::Settings.lon.to_s)
-  info_win.refresh
-end
-
-def self.updateLat
-  info_win = App::INFO_WIN
-  info_win.setpos(35,0)
-  info_win.clrtoeol
-  info_win.addstr(App::Settings.lat.to_s)
-  info_win.refresh
-end
-
-def self.updateGeo
-  # Help screen popup with command key list
-  win = Curses.stdscr
-  geowin = win.subwin(30,60,win.maxy / 2 - 15, win.maxx / 2 - 30)
-  geowin.box("|","-")
-  Curses.echo
-  Curses.curs_set(1)
-  geowin.setpos(2,2)
-  geowin.addstr("Enter your longitude as decimal degrees, West is negative")
-  geowin.setpos(3,2)
-  App::Settings.lon = geowin.getstr.to_f
-  while !App::Settings.lon.between?(-180,180)
-    geowin.setpos(3,2)
-    geowin.clrtoeol
-    geowin.setpos(2,2)
-    geowin.clrtoeol
-    geowin.addstr("Out of bounds, must be between -180 and 180, press any key")
-    geowin.getch
-    geowin.setpos(2,2)
-    geowin.clrtoeol
-    geowin.addstr("Enter your longitude as decimal degrees, West is negative")
-    geowin.setpos(3,2)
-    App::Settings.lon = geowin.getstr.to_f
+  def updateMag
+    @window.setpos(2,0)
+    @window.clrtoeol
+    @window.addstr(App::Settings.mag.to_s + " degrees")
+    @window.refresh
   end
-  MyStarsWindows.updateLon
-  geowin.setpos(4,2)
-  geowin.addstr("Enter your latitude as decimal degrees, West is negative")
-  geowin.setpos(5,2)
-  App::Settings.lat = geowin.getstr.to_f
-  while !App::Settings.lat.between?(-90,90)
-    geowin.setpos(5,2)
-    geowin.clrtoeol
-    geowin.setpos(4,2)
-    geowin.clrtoeol
-    geowin.addstr("Out of bounds, must be between -90 and 90, press any key")
-    geowin.getch
-    geowin.setpos(4,2)
-    geowin.clrtoeol
-    geowin.addstr("Enter your latitude as decimal degrees, West is negative")
-    geowin.setpos(5,2)
-    App::Settings.lat = geowin.getstr.to_f
+
+  def updateVisMag
+    @window.setpos(4,3)
+    @window.clrtoeol
+    @window.addstr(App::Settings.vis_mag.to_s) 
+    @window.refresh
   end
-  MyStarsWindows.updateLat
-  Curses.noecho
-  Curses.curs_set(0)
-  geowin.refresh
-  geowin.clear
-  geowin.refresh
-  geowin.close
-end
-
-def self.selectID
-  win = App::WIN
-  # Highlight the currently selected object
-  star = App::Settings.in_view.members.find { |object| object.id == App::Settings.selected_id }
-
-  star_selection_index = App::Settings.in_view.members.find_index(star)
-
-  if star
-    App::Settings.in_view.selected = star_selection_index
-    xpos, ypos = star.screen_coords(win)
-    win.setpos(ypos,xpos)
-    win.attrset(Curses::A_REVERSE)
-    win.addstr("*")
-    win.attrset(Curses::A_NORMAL)
-    win.refresh
-    MyStarsWindows.updateTargetInfo
-  end 
-end
-
-def self.selectNext
-  win = App::WIN
-  # Highlight the next object in current view
-  if App::Settings.in_view.members.empty?
-    return nil
+  
+  def updateLon
+    @window.setpos(33,0)
+    @window.clrtoeol
+    @window.addstr(App::Settings.lon.to_s)
+    @window.refresh
   end
-  # --- Deselect the previous one
-  star = App::Settings.in_view.members[App::Settings.in_view.selected]
-  xpos, ypos = star.screen_coords(win)
-  win.setpos(ypos,xpos)
-  win.attrset(Curses::A_NORMAL)
-  win.addstr("*")
-  # ---
-  if App::Settings.in_view.selected == App::Settings.in_view.members.length - 1
-    App::Settings.in_view.selected = 0
-  else
-    App::Settings.in_view.selected += 1
+  
+  def updateLat
+    @window.setpos(35,0)
+    @window.clrtoeol
+    @window.addstr(App::Settings.lat.to_s)
+    @window.refresh
   end
-  star = App::Settings.in_view.members[App::Settings.in_view.selected]
-  # Set targeted ID so we can highlight it again after refresh
-  App::Settings.selected_id = star.id
-  xpos, ypos = star.screen_coords(win)
-  win.setpos(ypos,xpos)
-  win.attrset(Curses::A_REVERSE)
-  win.addstr("*")
-  win.attrset(Curses::A_NORMAL)
-  win.refresh
-  MyStarsWindows.updateTargetInfo
-end
-
-def self.selectPrev
-  win = App::WIN
-  # Highlight the previous object in current view
-  if App::Settings.in_view.members.empty?
-    return nil
-  end
-  # ---Deselect the previous one
-  star = App::Settings.in_view.members[App::Settings.in_view.selected]
-  xpos, ypos = star.screen_coords(win)
-  win.setpos(ypos,xpos)
-  win.attrset(Curses::A_NORMAL)
-  win.addstr("*")
-  # ---
-  if (App::Settings.in_view.selected == 0) || (App::Settings.in_view.selected == -1)
-    App::Settings.in_view.selected = App::Settings.in_view.members.length - 1
-  else
-    App::Settings.in_view.selected -= 1
-  end
-  star = App::Settings.in_view.members[App::Settings.in_view.selected]
-  # Set targeted ID so we can highlight it again after refresh
-  App::Settings.selected_id = star.id
-  xpos, ypos = star.screen_coords(win)
-  win.setpos(ypos,xpos)
-  win.attrset(Curses::A_REVERSE)
-  win.addstr("*")
-  win.attrset(Curses::A_NORMAL)
-  win.refresh
-  MyStarsWindows.updateTargetInfo
-end
-
-def self.help
-  # Help screen popup with command key list
-  win = Curses.stdscr
-  helpwin = win.subwin(30,60,win.maxy / 2 - 15, win.maxx / 2 - 30)
-  helpwin.box("|","-")
-  helpwin.setpos(2,2)
-  helpwin.addstr("Arrow keys move around")
-  helpwin.setpos(3,2)
-  helpwin.addstr("(M) and (m) to filter by magnitude.")
-  helpwin.setpos(4,2)
-  helpwin.addstr("(+) and (-) to zoom in and out")
-  helpwin.setpos(5,2)
-  helpwin.addstr("Tab and Shift-Tab to cycle through visible objects")
-  helpwin.setpos(6,2)
-  helpwin.addstr("(c) to toggle constellation lines")
-  helpwin.setpos(7,2)
-  helpwin.addstr("(g) to toggle ground visibility")
-  helpwin.setpos(8,2)
-  helpwin.addstr("(L) to cycle label visibility level")
-  helpwin.setpos(9,2)
-  helpwin.addstr("(G) to input new geographic location")
-  helpwin.setpos(10,2)
-  helpwin.addstr("(q) to quit")
-  helpwin.refresh
-  helpwin.getch
-  helpwin.clear
-  helpwin.refresh
-  helpwin.close
 end
